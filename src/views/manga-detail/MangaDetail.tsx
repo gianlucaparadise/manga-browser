49636b1c19606a8aeb60bf7db90f1d09f63ac42b
gianlucaparadise <gianlucaparadise@gmail.com>
2021-04-02 19:50:47 +0200

    Manga detail page

--- src/views/manga-detail/MangaDetail.tsx
+++ src/views/manga-detail/MangaDetail.tsx
@@ -1,9 +1,70 @@
-import { SearchResult } from "../../models/SearchResponse";
+import { getMangaDetail } from "../../backend"
 import { useParams } from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import { MangaDetail as MangaDetailModel } from "../../models/MangaDetail";
+import { Chip, Paper, Typography } from "@material-ui/core";
+import styled from "styled-components";
+import { device } from "../styling";
 
-interface Props {
-    detail?: SearchResult
-}
+const MainContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 10px;
+`
+
+const Title = styled(Typography)`
+`
+
+const BodyContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+
+    @media ${device.mobile} {
+        flex-direction: column;
+        align-items: center;
+    }
+`
+
+interface CoverProps {
+    src: string | undefined
+};
+
+const Cover = styled(Paper)`
+    min-width: 40vw;
+    height: 60vw;
+    margin: 10px;
+    background-image: url(${(props: CoverProps) => props.src});
+    background-size: cover;
+    background-position: center;
+
+    @media ${device.mobile} {
+        width: 80vw;
+        height: 130vw;
+    }
+`
+
+const DetailContainer = styled.div`
+`
+
+const SynopsisBox = styled(Paper)`
+    padding: 50px 30px;
+    box-sizing: border-box;
+    text-align: start;
+`
+
+const AuthorsContainer = styled.div`
+    margin: 10px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+`
+
+const AuthorChip = styled(Chip)`
+    margin: 5px;
+`
 
 interface UrlParams {
     /**
@@ -12,13 +73,38 @@ interface UrlParams {
     id: string
 }
 
-const MangaDetail = (props: Props) => {
+const MangaDetail = () => {
     const { id }: UrlParams = useParams()
+    const [mangaDetail, setMangaDetail] = useState<MangaDetailModel>({})
+
+    useEffect(() => {
+        const getMangaDetailAndUpdate = async () => {
+            const manga = await getMangaDetail(id)
+            setMangaDetail(manga)
+        }
+
+        getMangaDetailAndUpdate()
+    }, [id, setMangaDetail])
 
     return (
-        <div>
-            {props.detail ? props.detail.title : `Title ${id}`}
-        </div>
+        <MainContainer>
+            <Title variant="h3">
+                {mangaDetail.title}
+            </Title>
+            <BodyContainer>
+                <Cover elevation={3} src={mangaDetail.image_url} />
+                <DetailContainer>
+                    <AuthorsContainer>
+                        {mangaDetail.authors?.map(a => <AuthorChip label={a.name} key={a.mal_id} />)}
+                    </AuthorsContainer>
+                    <SynopsisBox>
+                        <Typography variant="body1">
+                            {mangaDetail.synopsis}
+                        </Typography>
+                    </SynopsisBox>
+                </DetailContainer>
+            </BodyContainer>
+        </MainContainer>
     )
 }
 
